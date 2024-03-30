561bf6f77a1d7cb22c3fcbfa8c720209f9d1fffb
Boris Radovic <radovic.brs@gmail.com>
2024-03-30 12:19:06 +0300

    removed scripts

--- scripts/grid_search_scripts/common.sh
+++
@@ -1,8 +0,0 @@
-local_to_global_epochs_mapping() {
-    case "$1" in
-        1) global_epochs=120 ;;
-        2) global_epochs=60 ;;
-        4) global_epochs=32 ;;
-        *) global_epochs=0 ;;  # Default value if input is not 1, 2, or 4
-    esac
-}
